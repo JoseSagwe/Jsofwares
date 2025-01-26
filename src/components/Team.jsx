1dcdc80d0d8a08760233faafa49a4c670ec7e3c7
JoseSagwe <Josephsagwe421@gmail.com>
2025-01-26 19:29:50 +0300

    team

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,15 +6,15 @@ function Team() {
       <h1>Our Team</h1>
       <div className="team-list">
         <div className="team-member">
-          <h2>Joseph Sagwe</h2>
+          <h2>John Malcom</h2>
           <p>CEO</p>
         </div>
         <div className="team-member">
-          <h2>Mathew Bogonko</h2>
+          <h2>Silvia Hellen</h2>
           <p>CTO</p>
         </div>
         <div className="team-member">
-          <h2>Michael Owino</h2>
+          <h2>Michael Jack</h2>
           <p>Lead Developer</p>
         </div>
       </div>
